b931505558353f4980903c9b11c4c3a4b314263f
vinicius05051993 <viniciush.oliveira@outlook.com>
2024-08-21 15:07:47 -0300

    fix persistence

--- background.js
+++ background.js
@@ -2,23 +2,41 @@
 const secretTranslateKey = "AIzaSyD5eo942YXElyvkwo9GNe5jxpBDBPf-ftc";
 const supabaseUrl = "https://wgkakdbjxdqfdshqodtw.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indna2FrZGJqeGRxZmRzaHFvZHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2NjQ0NzcsImV4cCI6MjAzOTI0MDQ3N30.yAEn_IPXMxK4holhx9osY8nwHPVQIuF8bPZ7_asV0KM";
-let supabaseLoggedToken = false;
-let supabaseLoggedUserId = false;
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'getSecretTranslateKey') {
         sendResponse({ key: secretTranslateKey });
+
     } else if (request.action === 'supabaseUrl') {
         sendResponse({ key: supabaseUrl });
+
     } else if (request.action === 'supabaseKey') {
         sendResponse({ key: supabaseKey });
+
     } else if (request.action === 'supabaseLoggedToken') {
-        sendResponse({ key: supabaseLoggedToken });
+        chrome.storage.local.get(['supabaseLoggedToken'], (result) => {
+            sendResponse({ key: result.supabaseLoggedToken });
+        });
+        return true;
+
     } else if (request.action === 'supabaseLoggedUserId') {
-        sendResponse({ key: supabaseLoggedUserId });
-    }else if (request.action === 'saveLoggedToken') {
-        supabaseLoggedToken = request.value;
+        chrome.storage.local.get(['supabaseLoggedUserId'], (result) => {
+            sendResponse({ key: result.supabaseLoggedUserId });
+        });
+        return true;
+
+    } else if (request.action === 'saveLoggedToken') {
+        chrome.storage.local.set({ supabaseLoggedToken: request.value }, () => {
+            sendResponse({ key: 'supabaseLoggedToken', value: request.value });
+        });
+        return true;
+
     } else if (request.action === 'saveLoggedUserId') {
-        supabaseLoggedUserId = request.value;
+        chrome.storage.local.set({ supabaseLoggedUserId: request.value }, () => {
+            sendResponse({ key: 'supabaseLoggedUserId', value: request.value });
+        });
+        return true;
+
     } else {
         sendResponse({ error: 'Ação desconhecida' });
     }
