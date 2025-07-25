223224d38398bbde4d6352a4a0954a7ab8a5ac38
Vinicius Henrique <viniciush.oliveira@outlook.com>
2025-07-25 09:14:08 -0300

    improve

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
-const secretTranslateKey = "AIzaSyD5eo942YXElyvkwo9GNe5jxpBDBPf-ftc";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indna2FrZGJqeGRxZmRzaHFvZHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM2NjQ0NzcsImV4cCI6MjAzOTI0MDQ3N30.yAEn_IPXMxK4holhx9osY8nwHPVQIuF8bPZ7_asV0KM";
+const secretTranslateKey = "";
+const supabaseKey = "";
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'getSecretTranslateKey') {
